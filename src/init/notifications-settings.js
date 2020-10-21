ac441ed3e69510ca731907d3cd6a1aef95e984ca
Peponi <pep0ni@pm.me>
2020-10-21 14:06:51 +0200

    feat(notifications): add overwrite styles for elastic ui
    ref: S9

--- src/init/notifications-settings.js
+++ src/init/notifications-settings.js
@@ -1,21 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import NotificationSettings, {RequestNotificationSettings}  from '@aktionariat/notification-settings/dist/NotificationSettings.bundle.es.js'
+import EmailSubscription from '@aktionariat/email-subscription/dist/EmailSubscription.bundle.es.min.js'
+import TokenLevelSubscription, {TokenActivitySubscription} from '@aktionariat/token-activity-subscription/dist/TokenActivitySubscription.bundle.es.min.js'
+import NotificationSettings, {RequestNotificationSettings} from '@aktionariat/notification-settings/dist/NotificationSettings.bundle.es.min.js'
 import defaultProps from '@aktionariat/notification-settings/component/defaultProps.json'
-import ReadUrlVars from '@aktionariat/read-url-vars/dist/ReadUrlVars.bundle.es.js'
+import {parseUrlQueryToObject} from '@aktionariat/utility-js'
+
+const urlVars = parseUrlQueryToObject(window.location)
 
 const props = {...defaultProps, ...{
   debug: false,
   getUrl: `https://aktionariat.com/notificationsettings`,
-  postUrl: `https://aktionariat.com/updatenotificationsettings`
+  postUrl: `https://aktionariat.com/updatenotificationsettings`,
+  email: urlVars.email,
+  code: urlVars.code
 }}
 
-const Output = () => (
-  <ReadUrlVars>
-    <RequestNotificationSettings {...props}>
-      <NotificationSettings  />
-    </RequestNotificationSettings>
-  </ReadUrlVars>
-)
+const notificationEmail = document.querySelector('.notification__email')
+if(notificationEmail) notificationEmail.innerText = urlVars.email
 
-ReactDOM.render(<Output />, document.getElementById('notification-settings'));
+
+ReactDOM.render(
+  <RequestNotificationSettings {...props}>
+    <NotificationSettings>
+      <EmailSubscription subscriptionToggleLabel="Aktionariat Newsletter" />
+    </NotificationSettings>
+  </RequestNotificationSettings>
+  ,  document.getElementById('notification-settings__email-subscription'));
+
+ReactDOM.render(
+  <RequestNotificationSettings {...props}>
+    <NotificationSettings>
+      <TokenLevelSubscription />
+    </NotificationSettings>
+  </RequestNotificationSettings>
+  , document.getElementById('notification-settings__token-level-subscription'));
+
+ReactDOM.render(
+  <RequestNotificationSettings {...props}>
+    <NotificationSettings>
+      <TokenActivitySubscription />
+    </NotificationSettings>
+  </RequestNotificationSettings>
+  ,  document.getElementById('notification-settings__token-activity-subscription'));
